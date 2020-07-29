918f373f0ae352b1ab3f89e76cec8130174f5ef6
Filipe Tavares-Cadete <filipe@cadete.eu>
2020-07-29 22:25:51 +0000

    Loads data and creates scRNAseq object.

--- workspace.R
+++ workspace.R
@@ -1,15 +1,41 @@
 library(tercen)
 library(dplyr)
+library(scRNAseq)
+library(SingleCellExperiment)
+library(tidyr)
+library(scater)
+library(AnnotationDbi)
+library(org.Hs.eg.db)
 
-options("tercen.workflowId" = "wwww")
-options("tercen.stepId"     = "dddd")
+options("tercen.workflowId" = "7d6077b7fa4df6315a718714de00346e")
+options("tercen.stepId"     = "803c9fc0-7fe0-4597-ae03-cae0ca0fa092")
 
 getOption("tercen.workflowId")
 getOption("tercen.stepId")
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
-  ctx$addNamespace() %>%
-  ctx$save()
+(ctx = tercenCtx())
+
+count_matrix <- ctx$as.matrix()
+
+rownames(count_matrix) <- ctx$rselect()[[1]]
+colnames(count_matrix) <- ctx$cselect()[[1]]
+
+sce <- SingleCellExperiment(assays = list(counts = count_matrix))
+
+rowData(sce)$Symbol <- mapIds(org.Hs.eg.db,
+                     keys = row.names(sce),
+                     column="SYMBOL",
+                     keytype="ENSEMBL",
+                     multiVals="first")
+
+
+rowData(sce)$Chr <- mapIds(org.Hs.eg.db,
+                              keys = row.names(sce),
+                              column="CHR",
+                              keytype="ENSEMBL",
+                              multiVals="first")
+
+is.mito <- which(rowData(sce)$Chr == "MT")
+stats <- calculateQCMetrics(sce, subsets=list(Mito=is.mito))
+
+
