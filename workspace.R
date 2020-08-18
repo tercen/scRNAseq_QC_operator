2206e716ddf129375f8fc657512e2b899a30952f
Filipe Tavares-Cadete <filipe@cadete.eu>
2020-08-18 21:48:20 +0000

    WIP operator missing output.

--- workspace.R
+++ workspace.R
@@ -4,11 +4,9 @@ library(scRNAseq)
 library(SingleCellExperiment)
 library(tidyr)
 library(scater)
-library(AnnotationDbi)
-library(org.Hs.eg.db)
 
 options("tercen.workflowId" = "7d6077b7fa4df6315a718714de00346e")
-options("tercen.stepId"     = "803c9fc0-7fe0-4597-ae03-cae0ca0fa092")
+options("tercen.stepId"     = "de995bb0-620a-4e70-85c5-c74a3fdbe37b")
 
 getOption("tercen.workflowId")
 getOption("tercen.stepId")
@@ -17,25 +15,30 @@ getOption("tercen.stepId")
 
 count_matrix <- ctx$as.matrix()
 
-rownames(count_matrix) <- ctx$rselect()[[1]]
+rownames(count_matrix) <- ctx$rselect()[[2]]
 colnames(count_matrix) <- ctx$cselect()[[1]]
 
 sce <- SingleCellExperiment(assays = list(counts = count_matrix))
 
-rowData(sce)$Symbol <- mapIds(org.Hs.eg.db,
-                     keys = row.names(sce),
-                     column="SYMBOL",
-                     keytype="ENSEMBL",
-                     multiVals="first")
-
-
-rowData(sce)$Chr <- mapIds(org.Hs.eg.db,
-                              keys = row.names(sce),
-                              column="CHR",
-                              keytype="ENSEMBL",
-                              multiVals="first")
+rowData(sce)$Chr <- ctx$rselect()[[1]]
 
 is.mito <- which(rowData(sce)$Chr == "MT")
-stats <- calculateQCMetrics(sce, subsets=list(Mito=is.mito))
+stats <- calculateQCMetrics(sce, feature_controls = list(Mito=is.mito))
+high.mito <- isOutlier(stats$pct_counts_Mito, type="higher")
 
+qc.libsize <- isOutlier(stats$total_counts, log=TRUE, type="lower")
+qc.nexprs <- isOutlier(stats$total_features_by_counts, log=TRUE, type="lower")
 
+output <- tibble(ctx$cselect()[[1]],
+                 pct_counts_Mito = stats$pct_counts_Mito,
+                 library_size = stats$total_counts,
+                 n_feature_detected = stats$total_features_by_counts,
+                 passes_QC = !(qc.libsize | qc.nexprs | high.mito))
+colnames(output)[[1]] <- ctx$cnames[[1]]
+output$.ci <- 0:(nrow(output)-1)
+
+row_indexes <- ctx %>% select(.ci)
+output <- left_join(row_indexes, output)
+
+ctx$addNamespace(output) %>%
+  ctx$save()
